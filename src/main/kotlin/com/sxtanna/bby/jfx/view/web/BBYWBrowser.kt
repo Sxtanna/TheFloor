8cecbdc78b5ae49771d0cef3d87e40898e7229b0
Sxtanna <ranald.taylor@gmail.com>
2018-09-14 17:03:51 -0400

    Removed empty init, redundant show, and made main private

--- src/main/kotlin/com/sxtanna/bby/jfx/view/web/BBYWBrowser.kt
+++ src/main/kotlin/com/sxtanna/bby/jfx/view/web/BBYWBrowser.kt
@@ -5,25 +5,15 @@ import com.sxtanna.bby.jfx.view.BigView
 import com.teamdev.jxbrowser.chromium.Browser
 import com.teamdev.jxbrowser.chromium.javafx.BrowserView
 import javafx.scene.Node
-import javafx.scene.layout.Pane
-import tornadofx.add
 
-class BBYWBrowser(val main: MainWindow) : BigView {
+class BBYWBrowser(private val main: MainWindow) : BigView {
 
     val brow = Browser()
     val view = BrowserView(brow)
 
 
-    override fun init() {
-
-    }
-
     override fun node(): Node {
         return view
-    }
-
-    override fun show(pane: Pane) {
-        pane.add(view)
     }
 
 
