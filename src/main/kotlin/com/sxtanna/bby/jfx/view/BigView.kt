ee5363aa57f16e727bb38ee1d0f7ca64d4b08dc7
Sxtanna <ranald.taylor@gmail.com>
2018-09-14 17:02:44 -0400

    Updated BigView with more data

--- src/main/kotlin/com/sxtanna/bby/jfx/view/BigView.kt
+++ src/main/kotlin/com/sxtanna/bby/jfx/view/BigView.kt
@@ -2,13 +2,24 @@ package com.sxtanna.bby.jfx.view
 
 import javafx.scene.Node
 import javafx.scene.layout.Pane
+import tornadofx.add
 
 interface BigView {
 
-    fun init()
+    val name: String
+        get() = this::class.java.simpleName
+
+    val desc: String
+        get() = "big view named $name"
+
+
+    fun init() = Unit
 
     fun node(): Node
 
-    fun show(pane: Pane)
+
+    fun show(pane: Pane) {
+        pane.add(node())
+    }
 
 }
