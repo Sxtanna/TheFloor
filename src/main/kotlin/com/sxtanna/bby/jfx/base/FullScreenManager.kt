55a3130f9a1299828d9068472ac41c09bee7bf18
Sxtanna <ranald.taylor@gmail.com>
2018-09-14 17:00:44 -0400

    Removed bloat and useless code

--- src/main/kotlin/com/sxtanna/bby/jfx/base/FullScreenManager.kt
+++ src/main/kotlin/com/sxtanna/bby/jfx/base/FullScreenManager.kt
@@ -5,23 +5,12 @@ import javafx.scene.input.KeyCode
 
 class FullScreenManager(private val main: BBYWJfx) {
 
-    private lateinit var hint: String
-
-
     fun init() {
-        //hint = main.stage.fullScreenExitHint
-
-
         main.stage.fullScreenExitHint = "Press F11 to exit fullscreen"
 
         main.stage.scene.setOnKeyPressed {
             if (it.code == KeyCode.F11) main.stage.isFullScreen = main.stage.isFullScreen.not()
         }
-    }
-
-    fun stop() {
-        main.stage.fullScreenExitHint = hint
-        main.stage.scene.onKeyPressed = null
     }
 
 }
